ff11c7e8237308416851de9cf6873b1782c654a7
Javier Oliver <javieroliver@Javiers-MacBook-Air.local>
2024-12-09 19:16:07 +0100

    fljn

--- src/Content/Components/contact.tsx
+++ src/Content/Components/contact.tsx
@@ -54,29 +54,30 @@ const Contact = ()=>{
     }
 
     return(<>
-     <Flex width={'100vw'}  overflow={'hidden'} alignItems={'center'} position={'absolute'} height={'100vh'} bg='brand.gray_1' >
+     <Flex width={'100vw'}  overflow={'hidden'} alignItems={'center'} position={'absolute'} height={'100vh'}>
         
-        <Flex flex='1' height={'100%'} px='4vw' justifyContent={'center'} alignItems={'center'}  bgRepeat="no-repeat"   bgSize="cover" bgPosition="center" bgImage={'/images/mesh.png'}>
-          <Flex bg='brand.gray_1' maxW={'750px'} boxShadow={'xl'} borderRadius={'.5rem'}  p='45px' flexDir={'column'}> 
-            <Text as='h2' overflowWrap="break-word" whiteSpace="pre-wrap" textAlign={{ base: 'center',sm:'center', md: 'start' }} fontSize={'4xl'}  >
-              {t('Contact_Hero')}
-            </Text>
-            <Text mb='30px' as='h3' mt='30px' w='80%' fontSize={'md'}  color="gray.600" fontWeight={300}  overflowWrap="break-word" whiteSpace="pre-wrap">
-              <HighlightText text={ t('Contact_SubHero')} color1='gray.600' color2='rgb(5, 102, 255)' bold />
-            </Text>
-            <Text as='h3' mt='20px' w='80%' fontSize={'md'}  color="gray.600" fontWeight={300}  overflowWrap="break-word" whiteSpace="pre-wrap">
-            {t('Contact_Offer')}
-            </Text>
-            {bulletPointsIcons.map((icon, index) => (
-              <Flex key={`point-${index}`} gap='10px' mt='30px'>
-                <Icon color='brand.text_blue' boxSize={'20px'}  as={icon}/>
-                <Text as='h4'  mt='-5px' color='gray.600' fontSize={'sm'}>{t(`Contact_Point_${index + 1}`)}</Text>
-              </Flex>
-            ))}
-          </Flex>
+        <Flex flex='1' py='5vh'  height={'100vh'} bg='brand.gray_1' px='4vw' justifyContent={'center'} flexDir={'column'}    >
+            <Box maxH={'100%'}   overflow={'scroll'} > 
+              <Text as='h2'  overflowWrap="break-word" whiteSpace="pre-wrap" textAlign={{ base: 'center',sm:'center', md: 'start' }} fontSize={'4xl'}  >
+                {t('Contact_Hero')}
+              </Text>
+              <Text mb='30px' as='h3' mt='30px' w='80%' fontSize={'md'}  color="gray.600" fontWeight={300}  overflowWrap="break-word" whiteSpace="pre-wrap">
+                <HighlightText text={ t('Contact_SubHero')} color1='gray.600' color2='rgb(5, 102, 255)' bold />
+              </Text>
+              <Text as='h3' mt='20px' w='80%' fontSize={'md'}  color="gray.600" fontWeight={300}  overflowWrap="break-word" whiteSpace="pre-wrap">
+              {t('Contact_Offer')}
+              </Text>
+              {bulletPointsIcons.map((icon, index) => (
+                <Flex key={`point-${index}`} gap='10px' mt='30px'>
+                  <Icon color='brand.text_blue' boxSize={'20px'}  as={icon}/>
+                  <Text as='h4'  mt='-5px' color='gray.600' fontSize={'sm'}>{t(`Contact_Point_${index + 1}`)}</Text>
+                </Flex>
+              ))}
+            </Box>
+ 
         </Flex>
 
-        <Flex flex='1' position={'relative'} px='4vw' justifyContent={'center'} alignItems={'center'} >
+        <Flex flex='1'   position={'relative'} px='4vw'justifyContent={'center'} alignItems={'center'} >
       
           <Flex  opacity={!submited ? 0 : 1} pointerEvents={!submited ? 'none' : 'all'} transition="opacity 0.3s ease-in-out"  position='absolute'  maxW={'750px'} boxShadow={'lg'} borderRadius={'.5rem'}  p='45px' flexDir={'column'}   bgRepeat="no-repeat"    bgSize="cover" bgPosition="center" bgImage={'/images/mesh.png'}> 
             
@@ -95,8 +96,8 @@ const Contact = ()=>{
 
           </Flex>
  
-          <Flex  position='absolute'  opacity={submited ? 0 : 1} pointerEvents={submited ? 'none' : 'all'} transition="opacity 0.3s ease-in-out"  maxW={'800px'} justifyContent={'center'} alignItems={'center'} flexDir={'column'} flex='1'>
-            <FormInput placeholder={'Javier Sánchez'} field={t('ContactBox_1')} value={name} setValue={setName} />
+          <Flex  position='absolute'    opacity={submited ? 0 : 1} pointerEvents={submited ? 'none' : 'all'} transition="opacity 0.3s ease-in-out"  maxW={'800px'} justifyContent={'center'} alignItems={'center'} flexDir={'column'} flex='1'>
+            <FormInput placeholder={t('NameSurname')} field={t('ContactBox_1')} value={name} setValue={setName} />
             <FormInput  validPattern={/^[^\s@]+@[^\s@]+\.[^\s@]+$/} placeholder={'correo@ejemplo.com'} field={t('ContactBox_2')} value={mail} setValue={setMail}  mt='40px'/>   
             <Box width={'100%'}  mt='50px'>
               <Button isDisabled={ name === '' || mail === '' || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail)}color={'white'} width={'100%'} fontWeight={400}  bgGradient='linear(to-br, #58daf4, rgb(5, 102, 255))' _hover={{bgGradient:'linear(to-br, #58daf4, rgb(5, 12, 255))'}}  size={'lg'} onClick={handleSubmit} >{waitingSend?t('Waiting'):t('Send')}</Button> 
--- src/Content/Widgets/Input.tsx
+++ src/Content/Widgets/Input.tsx
@@ -15,7 +15,7 @@ const NormalInput = ({placeholder, value, setValue, field, validPattern=null, mt
             <Box mt={mt as string} flexDir={{base:'column',sm:'column', md:'row'}} justifyContent={{base:'space-between',md:'space-between'}}  alignItems={{base:'normal',sm:'normal', md:'center'}}>
                 <Text as='label'  htmlFor={`${placeholder}-input`}  color={'brand.clear_black'} fontWeight={'medium'} fontSize={'md'}>{field}</Text>
                 <Box mt={'10px'} position="relative">
-                    <Input    transition='border-bottom 0.3s ease-in-out' fontWeight={300} fontSize={'md'}  id={`${placeholder}-input`} onChange={handleChange} value={value} placeholder={placeholder}  border={'none'} px={0} borderBottom={`3px solid ${isNotValid ? 'red':'rgb(211, 219, 229)'}`} size='sm'  bg='brand.gray_1' focusBorderColor='transparent'   _focus={{border:'none', borderBottom: `3px solid ${'rgb(5, 102, 255)'}`}}/>
+                    <Input    transition='border-bottom 0.3s ease-in-out'  fontWeight={300} fontSize={'md'}  id={`${placeholder}-input`} onChange={handleChange} value={value} placeholder={placeholder}  border={'none'} px={0} borderBottom={`3px solid ${isNotValid ? 'red':'rgb(211, 219, 229)'}`} size='sm'  bg='transparent' focusBorderColor='transparent'   _focus={{border:'none', borderBottom: `3px solid ${'rgb(5, 102, 255)'}`}}/>
                 </Box> 
             </Box>
         </FormControl>
